4da4726b97f4cfc7882ada9eb71b2bb8fda5981e
jithumani <71173706+jithumani@users.noreply.github.com>
2021-05-21 01:10:00 +0530

    Updates to use the mysql instead of mysql server

--- product-catalog-service/src/main/java/com/rainbowforest/productcatalogservice/ProductCatalogServiceApplication.java
+++ product-catalog-service/src/main/java/com/rainbowforest/productcatalogservice/ProductCatalogServiceApplication.java
@@ -1,6 +1,7 @@
 package com.rainbowforest.productcatalogservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -8,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableJpaRepositories
+@EnableAutoConfiguration
 public class ProductCatalogServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductCatalogServiceApplication.class, args);
