df21682f9b29a94b1b876e105df199543c52d447
jithumani <71173706+jithumani@users.noreply.github.com>
2021-05-22 14:19:15 +0530

    Update EurekaServerApplication.java

--- eureka-server/src/main/java/com/rainbowforest/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/rainbowforest/eurekaserver/EurekaServerApplication.java
@@ -4,17 +4,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
-    }
-    
-    
-    @GetMapping(value = "/")
-    public String index() {
-        return "Offline Patch Backend Service is UP and Running";
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+}
+
+@RestController
+class Health {
+
+	@GetMapping(value = "/")
+	public String index() {
+		return "Offline Patch Backend Service is UP and Running";
+	}
 }
